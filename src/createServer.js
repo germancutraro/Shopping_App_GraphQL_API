4fca48e65f0b2b0466e5aedd2c6642f674d64647
German Cutraro <germancutraro@hotmail.com>
2020-03-28 19:28:22 -0300

    graphql server

---
+++ src/createServer.js
@@ -0,0 +1,20 @@
+const { GraphQLServer } = require('graphql-yoga');
+const Query = require('./resolvers/Query');
+const Mutation = require('./resolvers/Mutation');
+const db = require('./db');
+
+function createServer() {
+  return new GraphQLServer({
+    typeDefs: '/src/schema.graphql',
+    resolvers: {
+      Mutation,
+      Query
+    },
+    resolverValidationOptions: {
+      requireResolversForResolveType: false
+    },
+    context: req => ({ ...req, db })
+  });
+}
+
+module.exports = createServer;
