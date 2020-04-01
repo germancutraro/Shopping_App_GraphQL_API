820790ee6490fde2ba9e9ac54efaf16430f324e8
BautiCutraro <bautistacutraro@gmail.com>
2020-04-01 09:59:45 -0300

    me query

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const cookieParser = require('cookie-parser');
 require('dotenv').config({ path: '.env' });
-
+const cookieParser = require('cookie-parser');
+const jwt = require('jsonwebtoken');
 const createServer = require('./createServer');
 const db = require('./db');
 
@@ -8,6 +8,14 @@ const server = createServer();
 
 server.express.use(cookieParser());
 // TODO: populate current user
+server.express.use((req, res, next) => {
+  const { token } = req.cookies;
+  if (token) {
+    const { userId } = jwt.verify(token, process.env.APP_SECRET);
+    req.userId = userId;
+  }
+  next();
+});
 
 server.start(
   {
--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,16 @@ const { forwardTo } = require('prisma-binding');
 
 const Query = {
   items: forwardTo('db'),
-  item: forwardTo('db')
+  item: forwardTo('db'),
+  me(parent, args, ctx, info) {
+    if (!ctx.request.userId) return null;
+    return ctx.db.query.user(
+      {
+        where: { id: ctx.request.userId }
+      },
+      info
+    );
+  }
 };
 
 module.exports = Query;
